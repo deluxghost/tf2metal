6e77e7335714be697d72cf903a0ad9f266e3646d
DX <deluxghost@gmail.com>
2019-03-20 16:48:46 +0800

    implement RangeMetal

--- metal.py
+++ metal.py
@@ -6,7 +6,12 @@ from decimal import ROUND_DOWN
 import re
 import sys
 
-__version__ = '1.2.0'
+__version__ = '2.0.0'
+
+EXCHANGE_RATE = None
+
+decimal.setcontext(decimal.ExtendedContext)
+decimal.getcontext().prec = 18
 
 try:
     import colorama
@@ -25,35 +30,66 @@ operations = OrderedDict([
 symbols = operations.keys()
 
 
-class Metal(object):
+class Metal:
 
-    scrap = None
+    __scrap = None
 
     def __init__(self, ref='0', rec='0', scrap='0', weapon='0'):
-        self.scrap = D('0')
+        # init with 0 metal
+        self.__scrap = D('0')
+        # init infinity
+        all_currency = (D(ref), D(rec), D(scrap), D(weapon))
+        if D('inf') in all_currency:
+            self.__scrap += D('inf')
+        if D('-inf') in all_currency:
+            self.__scrap += D('-inf')
+        if self.__scrap == D('NaN') or D('NaN') in all_currency:
+            raise ValueError('Metal can not be NaN')
+        if self.__scrap != D('0'):
+            return
+        # load weapon arg
         weapon = D(weapon).quantize(D('1'), rounding=ROUND_DOWN)
-        self.scrap += weapon / D('2')
+        # put 0.5 scrap per weapon into metal storage
+        self.__scrap += weapon / D('2')
+        # load scrap arg, rounding to 0.5 scrap
         scrap = (D(scrap) * D('2')).quantize(D('1')) / D('2')
-        self.scrap += scrap
+        # put scrap into storage
+        self.__scrap += scrap
+        # rec3 stands for reclaimed rounding to 0.33
         rec3 = ((D(rec) * D('3')).quantize(D('1')) / D('3')).quantize(D('.01'), rounding=ROUND_DOWN)
+        # rec9 stands for reclaimed rounding to 0.11
         rec9 = ((D(rec) * D('9')).quantize(D('1')) / D('9')).quantize(D('.01'), rounding=ROUND_DOWN)
-        self.scrap += D('3') * (rec3 // D('1'))
-        self.scrap += rec3 % D('1') // D('0.33')
+        # put integer part of rec3 into storage, 3 scrap per reclaimed
+        self.__scrap += D('3') * (rec3 // D('1'))
+        # put decimal part of rec3 into storage, 1 scrap per 0.33 reclaimed
+        self.__scrap += rec3 % D('1') // D('0.33')
+        # adjust storage with 0.5 scrap, that's the part of reclaimed over 0.33
         if rec9 > rec3:
-            self.scrap += D('0.5')
+            self.__scrap += D('0.5')
         if rec9 < rec3:
-            self.scrap -= D('0.5')
+            self.__scrap -= D('0.5')
+        # ref9 stands for refined rounding to 0.11
         ref9 = ((D(ref) * D('9')).quantize(D('1')) / D('9')).quantize(D('.01'), rounding=ROUND_DOWN)
+        # ref18 stands for refined rounding to 0.05
         ref18 = ((D(ref) * D('18')).quantize(D('1')) / D('18')).quantize(D('.01'), rounding=ROUND_DOWN)
-        self.scrap += D('9') * (ref9 // D('1'))
-        self.scrap += ref9 % D('1') // D('0.11')
+        # put integer part of ref9 into storage, 9 scrap per refined
+        self.__scrap += D('9') * (ref9 // D('1'))
+        # put decimal part of ref9 into storage, 1 scrap per 0.11 reclaimed
+        self.__scrap += ref9 % D('1') // D('0.11')
+        # adjust storage with 0.5 scrap, that's the part of refined over 0.11
         if ref18 > ref9:
-            self.scrap += D('0.5')
+            self.__scrap += D('0.5')
         if ref18 < ref9:
-            self.scrap -= D('0.5')
+            self.__scrap -= D('0.5')
+
+    @property
+    def scrap(self):
+        return self.__scrap
 
     def strfref(self, fmt):
         """
+        formatting string:
+
         %w - metal value in weapon
         %s - metal value in scrap
         %c - metal value in reclaimed
@@ -64,15 +100,21 @@ class Metal(object):
         %R - refined amount in normalize form
         %% - % character
         """
-        scrap = self.scrap
-        ref_w = (scrap * D('2')).quantize(D('1'))
-        ref_W = (scrap % D('1') * D('2')).quantize(D('1'))
-        ref_s = scrap.quantize(D('.1'))
-        ref_S = (scrap % D('3')).quantize(D('1'), rounding=ROUND_DOWN)
-        ref_c = (scrap // D('3') + ref_S * D('0.33') + ref_W * D('0.16')).quantize(D('.01'))
-        ref_C = (scrap % D('9') // D('3')).quantize(D('.01'))
-        ref_r = (scrap // D('9') + ref_C * D('0.33') + ref_S * D('0.11') + ref_W * D('0.05')).quantize(D('.01'))
-        ref_R = (scrap // D('9')).quantize(D('.01'))
+        scrap = self.__scrap
+        if scrap.is_infinite():
+            ref_w = ref_W = scrap
+            ref_s = ref_S = scrap
+            ref_c = ref_C = scrap
+            ref_r = ref_R = scrap
+        else:
+            ref_w = (scrap * D('2')).quantize(D('1'))
+            ref_W = (scrap % D('1') * D('2')).quantize(D('1'))
+            ref_s = scrap.quantize(D('.1'))
+            ref_S = (scrap % D('3')).quantize(D('1'), rounding=ROUND_DOWN)
+            ref_c = (scrap // D('3') + ref_S * D('0.33') + ref_W * D('0.16')).quantize(D('.01'))
+            ref_C = (scrap % D('9') // D('3')).quantize(D('.01'))
+            ref_r = (scrap // D('9') + ref_C * D('0.33') + ref_S * D('0.11') + ref_W * D('0.05')).quantize(D('.01'))
+            ref_R = (scrap // D('9')).quantize(D('.01'))
         i = 0
         n = len(fmt)
         output = list()
@@ -116,103 +158,252 @@ class Metal(object):
         return self.strfref('%r ref')
 
     def __repr__(self):
-        return self.strfref('Metal(%r)')
+        if self.__scrap < 0:
+            return f'-Metal({-self.__scrap})'
+        return f'Metal({self.__scrap})'
 
     def __neg__(self):
-        data = -(self.scrap)
+        data = -(self.__scrap)
         return Metal(scrap=data)
 
     def __pos__(self):
-        return Metal(scrap=self.scrap)
+        return Metal(scrap=self.__scrap)
+
+    def __abs__(self):
+        return Metal(scrap=abs(self.__scrap))
 
     def __add__(self, other):
-        if isinstance(other, Metal):
-            data = self.scrap + other.scrap
-        elif isinstance(other, (int, float, D)) or isinstance(other, str):
-            other = Metal(other)
-            data = self.scrap + other.scrap
-        else:
-            return NotImplemented
-        return Metal(scrap=data)
+        if _is_single_metal(other):
+            scrap = self.__scrap + other.scrap
+            return Metal(scrap=scrap)
+        return NotImplemented
 
     def __radd__(self, other):
         return self.__add__(other)
 
     def __sub__(self, other):
-        if isinstance(other, Metal):
-            data = self.scrap - other.scrap
-        elif isinstance(other, (int, float, D)) or isinstance(other, str):
-            other = Metal(other)
-            data = self.scrap - other.scrap
-        else:
-            return NotImplemented
-        return Metal(scrap=data)
+        return self.__add__(-other)
 
     def __rsub__(self, other):
-        metal = self.__sub__(other)
-        metal.scrap = -(metal.scrap)
-        return metal
+        return -self.__sub__(other)
 
     def __mul__(self, other):
-        if isinstance(other, (int, float, D)) or isinstance(other, str):
-            other = D(other)
-            data = self.scrap * other
-        else:
-            return NotImplemented
-        return Metal(scrap=data)
+        if _is_number(other):
+            scrap = self.__scrap * D(other)
+            return Metal(scrap=scrap)
+        return NotImplemented
 
     def __rmul__(self, other):
         return self.__mul__(other)
 
     def __truediv__(self, other):
-        if isinstance(other, Metal):
-            data = self.scrap / other.scrap
-            data = normalize(data.quantize(D('.01')))
+        if _is_single_metal(other):
+            data = self.__scrap / other.scrap
+            if data.is_finite():
+                data = normalize(data.quantize(D('.01')))
             return data
-        elif isinstance(other, (int, float, D)) or isinstance(other, str):
-            other = D(other)
-            data = self.scrap / other
-            return Metal(scrap=data)
+        elif _is_number(other):
+            scrap = self.__scrap / D(other)
+            return Metal(scrap=scrap)
         return NotImplemented
 
-    def __div__(self, other):
-        return self.__truediv__(other)
+    def __rtruediv__(self, other):
+        if _is_single_metal(other):
+            return other.__truediv__(self)
+        return NotImplemented
 
     def __eq__(self, other):
-        if not isinstance(other, Metal):
-            return NotImplemented
-        return self.scrap == other.scrap
+        if _is_single_metal(other):
+            return self.__scrap == other.scrap
+        return NotImplemented
 
     def __ne__(self, other):
-        if not isinstance(other, Metal):
-            return NotImplemented
-        return self.scrap != other.scrap
+        if _is_single_metal(other):
+            return self.__scrap != other.scrap
+        return NotImplemented
 
     def __ge__(self, other):
-        if not isinstance(other, Metal):
-            return NotImplemented
-        return self.scrap >= other.scrap
+        if _is_single_metal(other):
+            return self.__scrap >= other.scrap
+        return NotImplemented
 
     def __le__(self, other):
-        if not isinstance(other, Metal):
-            return NotImplemented
-        return self.scrap <= other.scrap
+        if _is_single_metal(other):
+            return self.__scrap <= other.scrap
+        return NotImplemented
 
     def __gt__(self, other):
-        if not isinstance(other, Metal):
-            return NotImplemented
-        return self.scrap > other.scrap
+        if _is_single_metal(other):
+            return self.__scrap > other.scrap
+        return NotImplemented
 
     def __lt__(self, other):
-        if not isinstance(other, Metal):
-            return NotImplemented
-        return self.scrap < other.scrap
-
-    def __nonzero__(self):
-        return self.scrap > D('0')
+        if _is_single_metal(other):
+            return self.__scrap < other.scrap
+        return NotImplemented
 
     def __bool__(self):
-        return self.__nonzero__()
+        return self.__scrap != D('0')
+
+
+class RangeMetal(Metal):
+
+    __start = None
+    __end = None
+    __median = None
+
+    def __new__(cls, start, end):
+        if not _is_single_metal(start) or not _is_single_metal(end):
+            raise TypeError('RangeMetal accepts Metal arguments only')
+        if start == end:
+            return start
+        return super().__new__(cls)
+
+    def __init__(self, start, end):
+        if start > end:
+            start, end = end, start
+        self.__start = start
+        self.__end = end
+        self.__median = (start + end) / D('2')
+
+    @property
+    def start(self):
+        return self.__start
+
+    @property
+    def end(self):
+        return self.__end
+
+    @property
+    def median(self):
+        return self.__median
+
+    @property
+    def scrap(self):
+        raise NotImplementedError('RangeMetal does not support scrap property')
+
+    def strfref(self, *args, **kwargs):
+        raise NotImplementedError('RangeMetal does not support formatting string')
+
+    def __str__(self):
+        start = self.__start.strfref('%r')
+        end = self.__end.strfref('%r')
+        return f'{start} ~ {end} ref'
+
+    def __repr__(self):
+        start = repr(self.__start)
+        end = repr(self.__end)
+        return f'RangeMetal({start}, {end})'
+
+    def __iter__(self):
+        yield self.__start
+        yield self.__end
+
+    def __neg__(self):
+        return RangeMetal(-self.__end, -self.__start)
+
+    def __pos__(self):
+        return RangeMetal(self.__start, self.__end)
+
+    def __abs__(self):
+        return NotImplemented
+
+    def __add__(self, other):
+        if _is_single_metal(other):
+            start = self.__start + other
+            end = self.__end + other
+        elif isinstance(other, RangeMetal):
+            start = self.__start + other.start
+            end = self.__end + other.end
+        else:
+            return NotImplemented
+        return RangeMetal(start, end)
+
+    def __mul__(self, other):
+        if _is_number(other):
+            start = self.__start * D(other)
+            end = self.__end * D(other)
+            return RangeMetal(start, end)
+        return NotImplemented
+
+    def __truediv__(self, other):
+        if _is_single_metal(other):
+            start = self.__start / other
+            if start.is_finite():
+                start = normalize(start.quantize(D('.01')))
+            end = self.__end / other
+            if end.is_finite():
+                end = normalize(end.quantize(D('.01')))
+            if start == end:
+                return start
+            if start > end:
+                start, end = end, start
+            return (start, end)
+        elif isinstance(other, RangeMetal):
+            answers = [
+                self.__start / other.start,
+                self.__start / other.end,
+                self.__end / other.start,
+                self.__end / other.end
+            ]
+            start = min(answers)
+            end = max(answers)
+            if start.is_finite():
+                start = normalize(start.quantize(D('.01')))
+            if end.is_finite():
+                end = normalize(end.quantize(D('.01')))
+            if start == end:
+                return start
+            return (start, end)
+        elif _is_number(other):
+            start = self.__start / D(other)
+            end = self.__end / D(other)
+            return RangeMetal(start, end)
+        return NotImplemented
+
+    def __rtruediv__(self, other):
+        if _is_single_metal(other):
+            start = other / self.__start
+            if start.is_finite():
+                start = normalize(start.quantize(D('.01')))
+            end = other / self.__end
+            if end.is_finite():
+                end = normalize(end.quantize(D('.01')))
+            if start == end:
+                return start
+            if start > end:
+                start, end = end, start
+            return (start, end)
+        elif isinstance(other, RangeMetal):
+            return other.__truediv__(self)
+        return NotImplemented
+
+    def __eq__(self, other):
+        if _is_single_metal(other):
+            return False
+        if isinstance(other, RangeMetal):
+            if self.__start == other.start and self.__end == other.end:
+                return True
+            return False
+        return NotImplemented
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __ge__(self, other):
+        return NotImplemented
+
+    def __le__(self, other):
+        return NotImplemented
+
+    def __gt__(self, other):
+        return NotImplemented
+
+    def __lt__(self, other):
+        return NotImplemented
+
+    def __bool__(self):
+        return True
 
 
 class ParserError(Exception):
@@ -223,14 +414,26 @@ class ParserError(Exception):
 
     def __repr__(self):
         classname = self.__class__.__name__
-        return '{0}({1})'.format(classname, repr(self.message))
+        return f'{classname}({repr(self.message)})'
 
     def __str__(self):
         return self.message
 
 
+def _is_single_metal(obj):
+    return isinstance(obj, Metal) and not isinstance(obj, RangeMetal)
+
+
+def _is_number(obj):
+    try:
+        D(obj)
+        return True
+    except Exception:
+        return False
+
+
 def normalize(d):
-    return d.quantize(D('1')) if d == d.to_integral() else d.normalize()
+    return d.quantize(D('1')) if d.is_finite() and d == d.to_integral() else d.normalize()
 
 
 def convert(expr):
